8a6c872d5230fc172b7b9513ada23cb65ec4b466
bijoysingh <bijoysingh693@gmail.com>
2015-09-12 11:50:02 +0530

    Adding category UI

--- src/main/java/com/gymkhana/iitbeats/viewholder/CategoryViewHolder.java
+++ src/main/java/com/gymkhana/iitbeats/viewholder/CategoryViewHolder.java
@@ -2,13 +2,22 @@ package com.gymkhana.iitbeats.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gymkhana.iitbeats.R;
 
 /**
  * Created by BijoySingh on 9/12/2015.
  */
 public class CategoryViewHolder extends RecyclerView.ViewHolder {
 
+    public ImageView thumbnail;
+    public TextView title;
+
     public CategoryViewHolder(View view) {
         super(view);
+        thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        title = (TextView) view.findViewById(R.id.title);
     }
 }
