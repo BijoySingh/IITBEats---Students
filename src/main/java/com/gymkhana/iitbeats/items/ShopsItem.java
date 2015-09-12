97e984d308e6235b4f0e1912de0b5e4d28befe6e
bijoysingh <bijoysingh693@gmail.com>
2015-09-12 16:14:53 +0530

    Adding base code for shops and shop list

--- src/main/java/com/gymkhana/iitbeats/items/ShopsItem.java
+++ src/main/java/com/gymkhana/iitbeats/items/ShopsItem.java
@@ -1,8 +1,28 @@
 package com.gymkhana.iitbeats.items;
 
+import org.json.JSONObject;
+
 /**
  * Created by BijoySingh on 9/12/2015.
  */
 public class ShopsItem {
-    ;
+    public String title, address, phone, owner;
+    public Integer id;
+
+    public ShopsItem(JSONObject json) throws Exception {
+        id = json.getInt(JsonKeys.ID);
+
+        title = json.getString(JsonKeys.TITLE);
+        address = json.getString(JsonKeys.ADDRESS);
+        phone = json.getString(JsonKeys.PHONE);
+        owner = json.getString(JsonKeys.OWNER);
+    }
+
+    public static final class JsonKeys {
+        public static final String ID = "id";
+        public static final String TITLE = "title";
+        public static final String ADDRESS = "address";
+        public static final String PHONE = "phone";
+        public static final String OWNER = "owner";
+    }
 }
--- src/main/java/com/gymkhana/iitbeats/viewholder/ShopsViewHolder.java
+++ src/main/java/com/gymkhana/iitbeats/viewholder/ShopsViewHolder.java
@@ -2,13 +2,25 @@ package com.gymkhana.iitbeats.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gymkhana.iitbeats.R;
 
 /**
  * Created by BijoySingh on 9/12/2015.
  */
 public class ShopsViewHolder extends RecyclerView.ViewHolder {
+    TextView title, address, owner, phone;
+    ImageView status, call;
 
     public ShopsViewHolder(View view) {
         super(view);
+        title = (TextView) view.findViewById(R.id.title);
+        address = (TextView) view.findViewById(R.id.address);
+        owner = (TextView) view.findViewById(R.id.owner);
+        phone = (TextView) view.findViewById(R.id.phone);
+        status = (ImageView) view.findViewById(R.id.status);
+        call = (ImageView) view.findViewById(R.id.call);
     }
 }
