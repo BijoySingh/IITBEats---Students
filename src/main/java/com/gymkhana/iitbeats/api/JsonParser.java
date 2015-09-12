3e97c2b821199a6a499a836fdd7ccf0e7bb38509
bijoysingh <bijoysingh693@gmail.com>
2015-09-12 13:09:20 +0530

    Basic Json Parser code

--- src/main/java/com/gymkhana/iitbeats/api/JsonParser.java
+++ src/main/java/com/gymkhana/iitbeats/api/JsonParser.java
@@ -4,7 +4,14 @@ import android.util.Log;
 
 import com.gymkhana.iitbeats.fragments.RefreshBasedFragment;
 import com.gymkhana.iitbeats.items.ApiItem;
+import com.gymkhana.iitbeats.items.BillsItem;
+import com.gymkhana.iitbeats.items.CategoryItem;
+import com.gymkhana.iitbeats.items.MenuItem;
+import com.gymkhana.iitbeats.items.OrdersItem;
+import com.gymkhana.iitbeats.items.ShopsItem;
 import com.gymkhana.iitbeats.utils.DataType;
+
+import java.util.List;
 
 /**
  * Created by BijoySingh on 9/12/2015.
@@ -16,10 +23,38 @@ public class JsonParser {
 
         try {
             if (item.data_type == DataType.MENU) {
-
+                fragment.updateList(parseMenuItem(response));
+            } else if (item.data_type == DataType.CATEGORY) {
+                fragment.updateList(filterCategories(parseMenuItem(response)));
+            } else if (item.data_type == DataType.SHOPS) {
+                fragment.updateList(parseShopsItem(response));
+            } else if (item.data_type == DataType.ORDERS) {
+                fragment.updateList(parseOrdersItem(response));
+            } else if (item.data_type == DataType.BILLS) {
+                fragment.updateList(parseBillsItem(response));
             }
         } catch (Exception error) {
             Log.e(JsonParser.class.getSimpleName(), "Parsing Error", error);
         }
+    }
+
+    public static final List<MenuItem> parseMenuItem(String response) throws Exception {
+        return null;
+    }
+
+    public static final List<ShopsItem> parseShopsItem(String response) throws Exception {
+        return null;
+    }
+
+    public static final List<OrdersItem> parseOrdersItem(String response) throws Exception {
+        return null;
+    }
+
+    public static final List<BillsItem> parseBillsItem(String response) throws Exception {
+        return null;
+    }
+
+    public static final List<CategoryItem> filterCategories(List<MenuItem> list) {
+        return null;
     }
 }
