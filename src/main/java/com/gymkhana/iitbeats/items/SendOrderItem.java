363f8149c2ff603ebdb10162c583ca203ac31c38
bijoysingh <bijoysingh693@gmail.com>
2015-09-13 12:10:27 +0530

    Added send item

---
+++ src/main/java/com/gymkhana/iitbeats/items/SendOrderItem.java
@@ -0,0 +1,14 @@
+package com.gymkhana.iitbeats.items;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by BijoySingh on 9/13/2015.
+ */
+public class SendOrderItem implements Serializable {
+
+    List<SendOrderSubItem> items = new ArrayList<>();
+    String comments;
+}
---
+++ src/main/java/com/gymkhana/iitbeats/items/SendOrderSubItem.java
@@ -0,0 +1,14 @@
+package com.gymkhana.iitbeats.items;
+
+import java.util.List;
+
+/**
+ * Created by BijoySingh on 9/13/2015.
+ */
+public class SendOrderSubItem {
+    public MenuItem food;
+    public Integer quantity;
+    public List<Integer> toppings;
+
+
+}
